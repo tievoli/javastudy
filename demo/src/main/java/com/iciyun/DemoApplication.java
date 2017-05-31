02a26a6175a991f2206a838c18aa40d024f0bed4
tievoli <jiangbin@iciyun.com>
2017-06-01 01:19:41 +0800

    添加注释信息

--- demo/src/main/java/com/iciyun/DemoApplication.java
+++ demo/src/main/java/com/iciyun/DemoApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * 入口类.
+ * 
+ * @author Ryan Jiang
+ *
+ */
 @EnableEurekaServer
 @SpringBootApplication
 public class DemoApplication {
